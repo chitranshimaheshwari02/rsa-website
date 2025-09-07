763d1b8a0be1a800047189ffa039d31291055d59
Chitranshi Maheshwari <chitranshimaheshwari@Chitranshis-MacBook-Air.local>
2025-09-07 13:42:38 +0530

    Prepare for GitHub Pages deployment

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
+const basename = process.env.NODE_ENV === "production" ? "/rsa-website" : "/";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
