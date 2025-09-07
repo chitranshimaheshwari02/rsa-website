f836e8fddc18f7993e857f1561d4f07b8e40d9ee
Chitranshi Maheshwari <chitranshimaheshwari@Chitranshis-MacBook-Air.local>
2025-09-07 21:56:16 +0530

    Updated feature

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
-const basename = process.env.NODE_ENV === "production" ? "/rsa-website" : "/";
+// const basename = process.env.NODE_ENV === "production" ? "/rsa-website" : "/";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
