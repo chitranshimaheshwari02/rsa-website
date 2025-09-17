85239a47f2f7ca993bc61d47b31e58a20bc204b3
Chitranshi Maheshwari <chitranshimaheshwari@Chitranshis-MacBook-Air.local>
2025-09-17 13:55:15 +0530

    updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.scss";
 
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const handleLinkClick = () => {
+    setMenuOpen(false); 
+  };
+
   return (
     <nav className="navbar">
+      <div className="navbar-container">
+        <div className="logo">Raj Shrivastava & Associates</div>
 
-      <div className="logo">Raj Shrivastava & Associates</div>
+        <ul className={`nav-links ${menuOpen ? "open" : ""}`}>
+          <li><Link to="/" onClick={handleLinkClick}>Home</Link></li>
+          <li><Link to="/about" onClick={handleLinkClick}>About Us</Link></li>
+          <li><Link to="/people" onClick={handleLinkClick}>People</Link></li>
+          <li><Link to="/practice" onClick={handleLinkClick}>Practice Areas</Link></li>
+          <li><Link to="/awareness" onClick={handleLinkClick}>Awareness</Link></li>
+          <li><Link to="/media" onClick={handleLinkClick}>Media Coverage</Link></li>
+          <li><Link to="/testimonials" onClick={handleLinkClick}>Testimonials</Link></li>
+          <li><Link to="/contact" onClick={handleLinkClick}>Get in Touch</Link></li>
+        </ul>
 
-      <ul className="nav-links">
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/about">About Us</Link></li>
-        <li><Link to="/people">People</Link></li>
-        <li><Link to="/practice">Practice Areas</Link></li>
-        <li><Link to="/awareness">Awareness</Link></li>
-        <li><Link to="/media">Media Coverage</Link></li>
-        <li><Link to="/testimonials">Testimonials</Link></li>
-        <li><Link to="/contact">Get in Touch</Link></li>
-      </ul>
+        <div
+          className="hamburger"
+          onClick={() => setMenuOpen(!menuOpen)}
+        >
+          <div className="bar"></div>
+          <div className="bar"></div>
+          <div className="bar"></div>
+        </div>
+      </div>
     </nav>
   );
 };
